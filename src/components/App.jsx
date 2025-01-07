727e095469d3523b455e1adb13637abd524a4040
Andrey Isenko <aisenko87@gmail.com>
2025-01-07 22:37:29 +0200

    add book

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,15 @@
-import Product from "./Product";
 
-export default function App() {
+import { BookList, favouriteBooks } from "./Create/create";
+
+
+
+
+const App = () => {
   return (
-    <div>
-      <h1>Best selling</h1>
-
-      <Product
-        name="Tacos With Lime"
-        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-        price={10.99}
-      />
-      <Product
-        name="Fries and Burger"
-        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
-        price={14.29}
-      />
-    </div>
+    <>
+	  <h1>Books of the week</h1>
+      <BookList books={favouriteBooks} />
+    </>
   );
-}
+};
+export default App
---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,8 @@
+export function Product() {
+    return (
+      <div>
+        <h2>Cookies</h2>
+        <p>Price: 999 credits</p>
+      </div>
+    );
+  };
